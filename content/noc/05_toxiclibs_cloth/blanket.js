a413f64b7d7a2d488b4b9072d79ed7c1679d832e
Nitish Puri <npuri1903@gmail.com>
2022-07-19 16:55:58 +0530

    remove unnecessary files.

--- content/noc/05_toxiclibs_cloth/blanket.js
+++ content/noc/05_toxiclibs_cloth/blanket.js
@@ -1,3 +1,27 @@
+class Connection extends VerletSpring2D {
+  constructor(p1, p2, length, strength) {
+    super(p1, p2, length, strength);
+  }
+
+  display() {
+    stroke(200);
+    line(this.a.x, this.a.y, this.b.x, this.b.y);
+  }
+}
+
+class Particle extends VerletParticle2D {
+  constructor(pos) {
+    super(pos);
+  }
+
+  display() {
+    fill(127);
+    stroke(200);
+    strokeWeight(2);
+    ellipse(this.x, this.y, 16);
+  }
+}
+
 class Blanket {
   constructor(len, strength) {
     this.particles = [];
--- content/noc/05_toxiclibs_cloth/index.md
+++ content/noc/05_toxiclibs_cloth/index.md
@@ -6,8 +6,6 @@ libs :
     - toxiclibs.js
     - toxichelper.js
 scripts : 
-    - particle.js
-    - connection.js
     - blanket.js
     - sketch.js
 title: Toxiclibs Softbody Cloth
